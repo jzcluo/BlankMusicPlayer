e9e2e1b0a71445387f17523d509e48870ea1f9f2
jzcluo <jeffreyzcluo@gmail.com>
2017-02-28 21:48:11 -0500

    first test run. single tap, double tap, fling working.

--- app/src/main/java/com/zluo/blankmusicplayer/MainActivity.java
+++ app/src/main/java/com/zluo/blankmusicplayer/MainActivity.java
@@ -8,26 +8,36 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener ,
-        MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener{
+        GestureDetector.OnDoubleTapListener, MediaPlayer.OnPreparedListener,
+        MediaPlayer.OnCompletionListener {
     private GestureDetectorCompat gestureDetectorCompat;
     private Deque<File> songQueue;
     private Stack<File> playedSongs;
     private Random rand = new Random();
     private MediaPlayer currentSong;
     private MediaPlayer nextSong;
+    private List<File> tempMusicList;
     private File[] kgMusicFiles;
     private int musicFilesLength;
     private File currentSongFile;
     private File nextSongFile;
+    private boolean loopThisSong = false;
+    private int screenWidth;
+    private int screenHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,17 +45,35 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         setContentView(R.layout.activity_main);
 
         gestureDetectorCompat = new GestureDetectorCompat(this,this);
+        gestureDetectorCompat.setOnDoubleTapListener(this);
 
         songQueue = new LinkedList<>();
         playedSongs = new Stack<>();
 
+        tempMusicList = new ArrayList<>();
         kgMusicFiles = getMusicFiles();
-        musicFilesLength = kgMusicFiles.length;
+        for (File file : kgMusicFiles) {
+            if (file.getAbsolutePath().endsWith(".mp3") ||
+                    file.getAbsolutePath().endsWith(".flac")) {
+                tempMusicList.add(file);
+            }
+        }
+        musicFilesLength = tempMusicList.size();
+        kgMusicFiles = new File[musicFilesLength];
+        kgMusicFiles = tempMusicList.toArray(kgMusicFiles);
 
         //adding 10 song files to queue
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             songQueue.offer(kgMusicFiles[rand.nextInt(musicFilesLength)]);
         }
+        //adding 5 songs to stack in case user wants to prevent empty stack error
+        for (int i = 0; i < 5; i++) {
+            playedSongs.push(kgMusicFiles[rand.nextInt(musicFilesLength)]);
+        }
+
+        screenWidth = this.getResources().getDisplayMetrics().widthPixels;
+        screenHeight = this.getResources().getDisplayMetrics().heightPixels;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
     }
     @Override
@@ -53,16 +81,20 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         super.onStart();
 
         currentSong = new MediaPlayer();
-        nextSong = new MediaPlayer();
+        //nextSong = new MediaPlayer();
         currentSongFile = songQueue.poll();
+        Log.d("songfileexist",currentSongFile.toString());
+
+
         try {
             currentSong.setDataSource(currentSongFile.getAbsolutePath());
         } catch (IOException e) {
-            Log.i("erroronstart","error is in onStart");
+            Log.d("erroronstart", "error is in onSTART");
             e.printStackTrace();
         }
         currentSong.setOnPreparedListener(MainActivity.this);
         currentSong.setOnCompletionListener(MainActivity.this);
+
         currentSong.prepareAsync();
     }
 
@@ -101,11 +133,14 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
             nextSong.release();
             nextSong = null;
         }
+        playedSongs = null;
+        songQueue = null;
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
     }
 
 
@@ -118,27 +153,29 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public void onCompletion(MediaPlayer mediaPlayer) {
-        //start playing next song
-        nextSong.prepareAsync();
 
-        //Push currentsong to the played stack and setting current the next song that'll play
-        playedSongs.push(currentSongFile);
-        currentSongFile = nextSongFile;
+        if (loopThisSong) {
+            currentSong.start();
+        } else {
+            //start playing next song
+            nextSong.prepareAsync();
 
-        //Releasing current song and setting it to the next song
-        currentSong.release();
-        currentSong = nextSong;
-        //now nextsong is null
-        //nextSong = null;
-        nextSong.release();
-        //adding a new song file onto the queue
-        songQueue.offer(kgMusicFiles[rand.nextInt(musicFilesLength)]);
+            //Push currentsong to the played stack and setting current the next song that'll play
+            playedSongs.push(currentSongFile);
+            currentSongFile = nextSongFile;
 
-        //get next song
-        prepareNextSong();
+            //Releasing current song and setting it to the next song
+            currentSong.release();
+            currentSong = nextSong;
+            //now nextsong is null. can't release and reset because currentSong points to the same
+            nextSong = null;
+            //adding a new song file onto the queue
+            songQueue.offer(kgMusicFiles[rand.nextInt(musicFilesLength)]);
 
+            //get next song
+            prepareNextSong();
+        }
     }
-
 
     //self-defined methods for playing last or next song
     public void playNextSong() {
@@ -150,7 +187,11 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         //already set on prepared listener
         currentSong.prepareAsync();
 
+        nextSong = null;
+
         prepareNextSong();
+
+        songQueue.offer(kgMusicFiles[rand.nextInt(musicFilesLength)]);
 
     }
 
@@ -159,28 +200,34 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         songQueue.offerFirst(currentSongFile);
 
         currentSong.release();
+        currentSong = null;
+
+        currentSong = new MediaPlayer();
 
         currentSongFile = playedSongs.pop();
         try {
             currentSong.setDataSource(currentSongFile.getAbsolutePath());
         } catch (IOException e) {
-            Log.i("errorplayprevioussong","Error is in play previous song");
+            Log.d("errorplayprevioussong","Error is in play previous song");
             e.printStackTrace();
         }
         currentSong.setOnPreparedListener(MainActivity.this);
         currentSong.prepareAsync();
 
         nextSong.release();
-        //nextSong = null;
+        nextSong = null;
+
         prepareNextSong();
     }
 
     private void prepareNextSong() {
+        nextSong = new MediaPlayer();
         nextSongFile = songQueue.poll();
         try {
             nextSong.setDataSource(nextSongFile.getAbsolutePath());
         } catch (IOException e) {
             //maybe push the file back
+            Log.d("errorpreparenextSong","Error in prepare next song");
             e.printStackTrace();
         }
         nextSong.setOnPreparedListener(MainActivity.this);
@@ -193,6 +240,9 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     //Setting methods for touch events
     @Override
     public boolean onDown(MotionEvent motionEvent) {
+        Log.d("location","X:" + motionEvent.getX() + "  Y: "+motionEvent.getY());
+        Log.d("screen size","X:" + screenWidth + "  Y: "+screenHeight);
+
         return true;
     }
 
@@ -203,12 +253,7 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     @Override
     public boolean onSingleTapUp(MotionEvent motionEvent) {
-        //pause or start the music depending on the status
-        if (currentSong.isPlaying()) {
-            currentSong.pause();
-        } else {
-            currentSong.start();
-        }
+        //method moved to onsingletapconfirmed as double tap was made possible
         return true;
     }
 
@@ -217,7 +262,22 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         //up and down to adjust volume
         //also right and left to switch songs
         //if happened on top of screen -- adjust screen color to black or white
-
+        if (motionEvent.getY() < screenHeight*0.3 && motionEvent1.getY() <screenHeight*0.3) {
+            //adjust the brightness
+            Toast.makeText(this,"You hit right on",Toast.LENGTH_SHORT).show();
+        } else if (Math.abs(v1) > Math.abs(v)) {
+            //adjust volume
+            Toast.makeText(this,"Adjusting volume",Toast.LENGTH_SHORT).show();
+        } else {
+            //next or last song
+            if (motionEvent.getX() > motionEvent1.getX()) {
+                Toast.makeText(this,"OnScroll next song",Toast.LENGTH_SHORT).show();
+                playNextSong();
+            } else {
+                Toast.makeText(this,"OnScroll previous song",Toast.LENGTH_SHORT).show();
+                playPreviousSong();
+            }
+        }
 
         return true;
     }
@@ -230,11 +290,38 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     @Override
     public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
         //next song or previous song
-        if (motionEvent.getX() > motionEvent.getX()) {
-            playNextSong();
-        } else {
-            playPreviousSong();
+        //add in a minimum distance the user needs to fling
+        if (Math.abs(v) > Math.abs(v1)) {
+            if (v < 0) {
+                Toast.makeText(this,"OnFling next song",Toast.LENGTH_SHORT).show();
+                playNextSong();
+            } else {
+                Toast.makeText(this,"OnFling previous song",Toast.LENGTH_SHORT).show();
+                playPreviousSong();
+            }
         }
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        if (currentSong.isPlaying()) {
+            currentSong.pause();
+        } else {
+            currentSong.start();
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        loopThisSong = !loopThisSong;
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        Toast.makeText(this,"This song's on loop" + loopThisSong, Toast.LENGTH_SHORT).show();
         return true;
     }
 
@@ -247,7 +334,6 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     public File[] getMusicFiles() {
         File allStorageDir = new File(Environment.getExternalStorageDirectory().getAbsoluteFile(),"kgmusic");
         File downloadedMusicDir = new File(allStorageDir, "download");
-        File[] kgMusicFiles = downloadedMusicDir.listFiles();
-        return kgMusicFiles;
+        return downloadedMusicDir.listFiles();
     }
 }
